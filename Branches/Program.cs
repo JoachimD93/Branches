62da4f34fd54a4b0671327ced70cf9a72680f2ae
Christophe baert <chrisn10@outlook.be>
2022-05-25 21:45:12 +0200

    method

--- Branches/Program.cs
+++ Branches/Program.cs
@@ -9,5 +9,10 @@ namespace Branches
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello c!");
         }
+
+        static void aName()
+        {
+
+        }
     }
 }
